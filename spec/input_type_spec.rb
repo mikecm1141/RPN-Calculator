2085babd65a535091f7a5d3d487af7584333f107
Mike <mikecm@gmail.com>
2019-03-16 16:36:05 -0600

    Create InputError::ValidOperator class
    This class and its method #=== will take a the given input string and
    check if its a valid operator. It will fail if the operator is not one
    of the basic elementary arithmetic operators.

--- spec/input_type_spec.rb
+++ spec/input_type_spec.rb
@@ -10,9 +10,28 @@ describe InputType do
         expect(InputType::ValidOperand.===('-3')).to be true
         expect(InputType::ValidOperand.===('10.5')).to be true
         expect(InputType::ValidOperand.===('-6.3')).to be true
+      end
 
+      it 'fails if given input string is not a valid operand' do
         expect(InputType::ValidOperand.===('text')).to be false
         expect(InputType::ValidOperand.===('?%#3')).to be false
+      end
+    end
+  end
+
+  describe InputType::ValidOperator do
+    context '.===(input)' do
+      it 'compares given input string to check for valid operation' do
+        expect(InputType::ValidOperator.===('+')).to be_truthy
+        expect(InputType::ValidOperator.===('-')).to be_truthy
+        expect(InputType::ValidOperator.===('/')).to be_truthy
+        expect(InputType::ValidOperator.===('*')).to be_truthy
+      end
+
+      it 'fails if given input is not a valid operation' do
+        expect(InputType::ValidOperator.===('$')).to be_falsey
+        expect(InputType::ValidOperator.===('&')).to be_falsey
+        expect(InputType::ValidOperator.===('%')).to be_falsey
       end
     end
   end
