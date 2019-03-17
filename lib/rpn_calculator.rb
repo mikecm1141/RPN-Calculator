11782002bf2e8d9afbf5af5a7ad0ef03971eb788
Mike <mikecm@gmail.com>
2019-03-16 19:36:28 -0600

    Add basic looping code and menu, parse user input each loop

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -4,5 +4,27 @@ require_relative 'calculator'
 
 class RpnCalculator
   def self.run
+    calculator = Calculator.new
+    puts <<-HEADERMSG
+
+    Welcome to the RPN calculator. See the README for more information on
+    proper usage of this application.
+
+    Enter 'c' to clear the current operand stack.
+    Enter 'v' to view the current operand stack.
+    Enter 'z' to remove the last number on the stack.
+    Enter 'q' to quit the application.
+
+    HEADERMSG
+    loop do
+      print '$:> '
+      user_input = STDIN.gets.chomp.downcase
+      if user_input == 'q'
+        puts 'Terminating application, goodbye'
+        exit
+      end
+
+      p calculator.parse_input(user_input)
+    end
   end
 end
